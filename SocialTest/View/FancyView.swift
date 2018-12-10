a65a83c3cf62d0585b5c4d1a8a6cff96e9d10b36
Globes Design <globes@Globess-iMac.local>
2018-12-10 09:56:19 +0200

    Feed part 1 complete

---
+++ SocialTest/View/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  SocialTest
+//
+//  Created by Globes Design on 10/12/2018.
+//  Copyright © 2018 Globes Design. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
